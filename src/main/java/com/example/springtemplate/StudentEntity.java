a73d16a8762e31fdf9ce9880b42048a6e9fbdf97
unknown <fkschon@gmail.com>
2022-05-09 15:55:35 -0400

    remove her

--- src/main/java/com/example/springtemplate/StudentEntity.java
+++
@@ -1,8 +0,0 @@
-package com.example.springtemplate;
-
-import javax.persistence.Entity;
-
-
-@Entity
-public class StudentEntity {
-}
--- src/main/java/com/example/springtemplate/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.example.springtemplate;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//import java.util.UUID;
-//
-//public class StudentRepository extends CrudRepository<StudentEntity, UUID>{
-//
-//}
