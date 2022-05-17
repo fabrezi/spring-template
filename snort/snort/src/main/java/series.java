6911ac4cbb0312660d16df0f23165c2275a236df
unknown <fkschon@gmail.com>
2022-05-17 12:40:08 -0400

    junk-code

---
+++ snort/snort/src/main/java/series.java
@@ -0,0 +1,12 @@
+public class series {
+
+    //make a insert sort
+    public static void main(String[] args){
+        int x1 = 0;
+        int y1 = 0;
+
+        z = x1 + y1 + R;
+
+        System.out.println("the number of the arranged value is:" + z);
+    }
+}
